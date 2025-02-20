1436095bea3278e12a677ee014d9ee2848ec5422
mattbedford <matt.bedford@gmail.com>
2025-02-20 17:51:25 +0100

    Reload from stash

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,8 @@
 
-<div class="footer">
-    &copy; <?php print date("Y");?>
-</div>
+<footer class="container">
+    <p class="float-end"><a href="#">Back to top</a></p>
+    <p>&copy; <?php echo date('Y'); ?> SLK construction & design Ltd. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+</footer>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 
