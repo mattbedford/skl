d6a5e2d0fba176df58eaaa3962cd90075deee6dd
mattbedford <matt.bedford@gmail.com>
2025-02-21 09:10:04 +0100

    add favicons etc

--- includes/head.php
+++ includes/head.php
@@ -4,6 +4,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php print get_page_title() ;?></title>
     <!--- TODO: Add a system to import or create meta data for SEO here --->
+    <link rel="apple-touch-icon" sizes="180x180" href="../img/icons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../img/icons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../imgicons//favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Source+Sans+3:ital,wght@0,200..900;1,200..900&display=swap');
     </style>
